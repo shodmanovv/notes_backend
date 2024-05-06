c8242a5a5dbb8e0b1e54544a057732e9b4c1bf60
Wertazus <pshodmanovv@gmail.com>
2024-05-06 14:56:18 +0500

    Initial commit

---
+++ user/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator, UniqueTogetherValidator
+from django.contrib.auth.models import User
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password2',]
+
+    def save(self):
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+
+        if password != password2:
+            raise serializers.ValidationError({'error': 'Password doesnt macht'})
+        
+        if User.objects.filter(email=self.validated_data['email']).exists():
+            raise serializers.ValidationError({'error': 'Email alrady exits'})
+        
+        account = User(
+            email=self.validated_data['email'],
+            username=self.validated_data['username']
+            )
+        account.set_password(self.validated_data['password'])
+        account.save()
+        return account
+
+def check_name(value):
+    if len(value) <= 5:
+        raise serializers.ValidationError("Name is too short")  
---
+++ user/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from . import views
+
+urlpatterns = [
+    path('login/', TokenObtainPairView.as_view(), name='token_abtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("logout/", views.logout_user, name="logout"),
+    path("signup/", views.user_register_view, name="signup"),
+    path("me/", views.me, name="me")
+]

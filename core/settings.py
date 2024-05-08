e17c1a357696d5484b6c0520993dcd1c45132120
Wertazus <pshodmanovv@gmail.com>
2024-05-08 16:16:52 +0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -45,17 +45,41 @@ MIDDLEWARE = [
 
 SITE_ID = 1
 
-CORS_ORIGIN_WHITELIST = [
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
+CORS_ORIGIN_WHITELIST = [
+    "https://notes-backend-iota-rosy.vercel.app"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-
+    "https://notes-backend-iota-rosy.vercel.app"
 ]
 
 CSRF_ALLOWED_ORIGINS = [
-
+    "https://notes-backend-iota-rosy.vercel.app"
 ]
+
+SESSION_COOKIE_DOMAIN = "https://notes-backend-iota-rosy.vercel.app"
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+
+SECURE_HSTS_SECONDS = 3600
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
 
 
 ROOT_URLCONF = 'core.urls'
